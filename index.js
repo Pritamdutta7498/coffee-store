6eece69f6aa9a91dc4691910a3a3f67ba00be8ca
PritamDutta <webdevpritam7498@gmail.com>
2023-05-31 17:34:22 +0600

    main setup is complete

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ app.use(cors());
 app.use(express.json());
 
 
-//coffeeMaster
-//QYa3IJ9ddMHKELHP
 
 console.log(process.env.DB_USER);
 
 console.log(process.env.DB_PASS);
 
-const uri = "mongodb+srv://coffeeMaster:QYa3IJ9ddMHKELHP@cluster0.zynq1cd.mongodb.net/?retryWrites=true&w=majority";
-// const uri = "mongodb+srv://<username>:<password>@cluster0.zynq1cd.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zynq1cd.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
